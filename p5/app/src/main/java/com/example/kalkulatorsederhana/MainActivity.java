b2bc11a9beaf02bb7b80e056acbf87179ef2168d
oniauliya99 <oniauliya99@gmail.com>
2021-08-06 13:37:25 +0700

    update logic

--- p5/app/src/main/java/com/example/kalkulatorsederhana/MainActivity.java
+++ p5/app/src/main/java/com/example/kalkulatorsederhana/MainActivity.java
@@ -38,48 +38,45 @@ public class MainActivity extends AppCompatActivity {
         btnKali = findViewById(R.id.btnKali);
         btnKurang = findViewById(R.id.btnKurang);
         btnBersih=findViewById(R.id.btnBersih);
+
         btnTambah.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                angka1= Float.parseFloat(editTextAngka1.getText().toString());
-                angka2=Float.parseFloat(editTextAngka2.getText().toString());
-                hasil=angka1+angka2;
-                result1 = "Hasil Penjumlahan = "+ hasil;
-                textViewHasil.setText(String.valueOf(result1));
-
+                if (cekAngka()){
+                    hasil=angka1+angka2;
+                    result1 = "Hasil Penjumlahan = "+ hasil;
+                    textViewHasil.setText(String.valueOf(result1));
+                }
             }
         });
         btnKurang.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                angka1= Float.parseFloat(editTextAngka1.getText().toString());
-                angka2=Float.parseFloat(editTextAngka2.getText().toString());
-                hasil=angka1-angka2;
-                result2 = "Hasil Pengurangan = "+hasil;
-                textViewHasil.setText(String.valueOf(result2));
-
+                if(cekAngka()){
+                    hasil=angka1-angka2;
+                    result2 = "Hasil Pengurangan = "+hasil;
+                    textViewHasil.setText(String.valueOf(result2));
+                }
             }
         });
         btnKali.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                angka1= Float.parseFloat(editTextAngka1.getText().toString());
-                angka2=Float.parseFloat(editTextAngka2.getText().toString());
-                hasil=angka1*angka2;
-                result3 = "Hasil Perkalian = "+hasil;
-                textViewHasil.setText(String.valueOf(result3));
-
+                if(cekAngka()){
+                    hasil=angka1*angka2;
+                    result3 = "Hasil Perkalian = "+hasil;
+                    textViewHasil.setText(String.valueOf(result3));
+                }
             }
         });
         btnBagi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                angka1= Float.parseFloat(editTextAngka1.getText().toString());
-                angka2=Float.parseFloat(editTextAngka2.getText().toString());
-                hasil=angka1/angka2;
-                result4 = "Hasil Pembagian = "+hasil;
-                textViewHasil.setText(String.valueOf(result4));
-
+                if(cekAngka()){
+                    hasil=angka1/angka2;
+                    result4 = "Hasil Pembagian = "+hasil;
+                    textViewHasil.setText(String.valueOf(result4));
+                }
             }
         });
         btnBersih.setOnClickListener(new View.OnClickListener() {
@@ -89,11 +86,27 @@ public class MainActivity extends AppCompatActivity {
                 editTextAngka2.setText(null);
                 hasil=0f;
                 textViewHasil.setText(String.valueOf(hasil));
-
-
             }
         });
 
+    }
+    public boolean cekAngka(){
+        editTextAngka1=findViewById(R.id.editTextAngka1);
+        editTextAngka2=findViewById(R.id.editTextAngka2);
+        textViewHasil=findViewById(R.id.labelHasil);
+
+        String cekData=editTextAngka1.getText().toString();
+        String cekData2=editTextAngka2.getText().toString();
+
+        if(cekData.equals("")||cekData2.equals("")){
+            String error = "Masukkan Kedua Angka!!";
+            textViewHasil.setText(error);
+            return false;
+        } else {
+            angka1=Float.parseFloat(cekData);
+            angka2=Float.parseFloat(cekData2);
+        }
+        return true;
     }
 
 }
